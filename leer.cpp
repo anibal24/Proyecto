6ea5cda0fbf4e28695f77fcefdacae70183d19b2
anibal24 <55145432+anibal24@users.noreply.github.com>
2019-10-31 07:19:14 -0500

    leer.cpp

---
+++ leer.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<wchar.h>
+#include<locale.h>
+#include<windows.h>
+using namespace std;
+
+string rutaArchivo;
+
+void leer(){
+	cout <<"Ingrese la ruta del archivo que desea leer: "; cin >> rutaArchivo;
+    char cRutaArchivo [rutaArchivo.size()];
+    strcpy(cRutaArchivo,rutaArchivo.c_str());
+    ifstream file;
+    file.open(cRutaArchivo);
+    string myStr;
+    ofstream archivo;
+    archivo.open("texto.txt",ios::out);
+    if(file.is_open()){
+        while(getline(file,myStr)){
+            cout << myStr;
+            archivo<<myStr;
+        }
+        cout <<"\nSe esta leyendo el archivo!\n";
+    }
+    else
+        cout <<"No se pudo abrir el archivo!\n";
+}
